8089fff0d062bbfa7dad15b74db59796c67ed0bd
Rodrigo <rodrigosurita@live.com>
2020-02-27 23:14:44 -0300

    Website fix

--- web-site-chatbot-example/js/init.js
+++ web-site-chatbot-example/js/init.js
@@ -7,7 +7,7 @@
 })(jQuery); // end of jQuery name space
 
 window.loadWatsonAssistantChat({
-  integrationID: "d707076d-271b-4f5f-bc9d-a82465ba2ce9", // The ID of this integration.
+  integrationID: "INTEGRATION_ID", // The ID of this integration.
   region: "us-south" // The region your integration is hosted in.
 }).then(function (instance) {
   instance.render();
